a0f86cfbb62a385364d5bdca43b990a0d25d2896
Yoba San <xmetalist@gmail.com>
2022-07-19 12:55:52 +0300

    Initial commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0w$2)x@&4+!^!ti4!@!4&n@5xa$g&72%)n6$ww!0@%jwrg$ljt"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
